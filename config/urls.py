7512d5638001e74507438d7356fd861ca7770a4c
Adnan Alam <adnan.alam@rfera.asia>
2022-06-27 10:52:40 +0600

    project urls.py updated with admin urls and apps urls

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.views import LogoutView
+from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", include("account.urls")),
+    path("", include("blog.urls")),
+    path("admin/", admin.site.urls),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
