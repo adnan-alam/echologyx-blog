f882b3edbef32eb792c7477245981cf6d410c998
Adnan Alam <adnan.alam@rfera.asia>
2022-06-29 02:10:40 +0600

    added Post and Comment views

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,32 @@ from blog import views as views_blog
 app_name = "blog"
 
 urlpatterns = [
-    path("", views_blog.HomeView.as_view(), name="home"),
+    # blog urls
+    path("", views_blog.PostListView.as_view(), name="home"),
+    path("post/<slug:slug>", views_blog.PostDetailView.as_view(), name="post_detail"),
+    path("post/create", views_blog.PostCreateView.as_view(), name="post_create"),
+    path(
+        "post/update/<slug:slug>",
+        views_blog.PostUpdateView.as_view(),
+        name="post_update",
+    ),
+    path(
+        "post/delete/<slug:slug>",
+        views_blog.PostDeleteView.as_view(),
+        name="post_delete",
+    ),
+    # comment urls
+    path(
+        "comment/create", views_blog.CommentCreateView.as_view(), name="comment_create"
+    ),
+    path(
+        "comment/update/<int:id>",
+        views_blog.CommentUpdateView.as_view(),
+        name="comment_update",
+    ),
+    path(
+        "comment/delete/<int:id>",
+        views_blog.CommentDeleteView.as_view(),
+        name="comment_delete",
+    ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,43 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    DetailView,
+)
+from .models import Post, Comment
 
 
-class HomeView(LoginRequiredMixin, TemplateView, View):
+class PostListView(LoginRequiredMixin, ListView):
+    model = Post
+    queryset = Post.objects.all().order_by("-created_at")
     template_name = "blog/home.html"
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+
+
+class CommentUpdateView(LoginRequiredMixin, UpdateView):
+    model = Comment
+
+
+class CommentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Comment
