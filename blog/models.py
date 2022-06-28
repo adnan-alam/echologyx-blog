1d2e922149975a1eb9415fea93024346ef8a1b39
Adnan Alam <adnan.alam@rfera.asia>
2022-06-29 00:26:37 +0600

    added Post and Comment models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
+from ckeditor.fields import RichTextField
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, verbose_name=_("User"), on_delete=models.CASCADE)
+    content = RichTextField(verbose_name=_("Content"))
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id} | {self.author}"
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, verbose_name=_("User"), on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, verbose_name=_("Post"), on_delete=models.CASCADE)
+    content = RichTextField(verbose_name=_("Content"))
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id} | {self.author}"
