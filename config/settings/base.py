fb1c5b2a3b544224d7008bcfae4a134a125452d5
Adnan Alam <adnan.alam@rfera.asia>
2022-06-29 01:14:41 +0600

    added missing ckeditor app in INSTALLED_APPS

--- config/settings/base.py
+++ config/settings/base.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sites",
+    # 3rd party apps
+    "ckeditor",
     # custom apps
     "account",
     "blog",
